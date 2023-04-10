ef9b5dfc49a77f0599b86054479388839e07fbbd
Ammar Jaber <jaberr.ammar22@gmail.com>
2023-04-11 00:40:32 +0300

    Lecture 12

---
+++ Lecture13/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Lecture13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblAge = new System.Windows.Forms.Label();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(92, 158);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(159, 20);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Enter Your Name";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(256, 154);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(341, 29);
+            this.txtName.TabIndex = 1;
+            this.txtName.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtName_KeyPress);
+            // 
+            // lblAge
+            // 
+            this.lblAge.AutoSize = true;
+            this.lblAge.Location = new System.Drawing.Point(95, 195);
+            this.lblAge.Name = "lblAge";
+            this.lblAge.Size = new System.Drawing.Size(143, 20);
+            this.lblAge.TabIndex = 2;
+            this.lblAge.Text = "Enter Your Age";
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(256, 194);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(341, 29);
+            this.txtAge.TabIndex = 3;
+            this.txtAge.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtAge_KeyPress);
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(256, 238);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(341, 49);
+            this.btnSubmit.TabIndex = 4;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(702, 450);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.lblAge);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblName);
+            this.Font = new System.Drawing.Font("Yet R", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lblAge;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.Button btnSubmit;
+    }
+}
+
---
+++ Lecture13/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lecture13
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtName_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // if the key wasn't letter or spetial charachter ignor it 
+            if (!char.IsControl(e.KeyChar) && !char.IsLetter(e.KeyChar)) 
+                e.Handled = true;
+        }
+
+        private void txtAge_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // if the key wasn't Digit or spetial charachter ignor it 
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+                e.Handled = true;
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtName.Text) || string.IsNullOrEmpty(txtAge.Text))
+            {
+                MessageBox.Show("Please enter valid data in all fields");
+            }
+            else
+            {
+                string name = txtName.Text;
+                int age = int.Parse(txtAge.Text);
+            }
+        }
+    }
+}
